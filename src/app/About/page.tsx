de94ee74e3e436778b7e36f933c646d675f871c3
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2024-09-22 14:39:29 +0500

    improved

---
+++ src/app/About/page.tsx
@@ -0,0 +1,28 @@
+export default function About() {
+    return ( 
+    <div> className=bg-Black-300
+        <h2> Hello! I’m Asadullah Shafique.</h2>
+         <h5>About Me:</h5> 
+         <p>  As a Full Stack Developer, I have honed my skills across both front-end and back-end development, ensuring comprehensive solutions for complex web applications.
+                        
+                    </p>
+                    <h6>Expertise:</h6> 
+         <ul>
+                
+                <li>Full-Stack Developer | Python | Next.js | Typescript </li>
+                <li>Freelancer | New Texchnology Enthusiastic</li>
+                <li>Content Writing | Statistical Analysis </li>
+                <li>Digital Marketer | Video Editor </li>
+                <li>Microsoft 365 | Communication Expertise </li>
+                <li>Authentication | Database Management  </li>
+                  </ul>
+                  <h6>My service are available as an expert Freelancer.</h6>
+                    
+            
+        
+            </div>
+    
+    );
+  }
+
+
---
+++ src/app/Contact/page.tsx
@@ -0,0 +1,17 @@
+export default function Contact() {
+  return (
+    <div>
+        <h2>Contact Us</h2>
+           <p><b>Contact Person:</b> Asadullah Shafique</p>
+           <p><b>Whatsapp:</b> +92 321 377 1445</p>
+           <p><b>Email:</b> <a href="mailto:asadullahshafique@hotmail.com">asadullahshafique@hotmail.com</a></p>
+           <p><b>LinkedIn:</b> <a href="https://www.linkedin.com/in/asadullah-shafique-a00679325" target="_blank">www.LinkedIn.com</a></p>
+           
+
+    </div>
+  );
+}
+
+
+
+         
